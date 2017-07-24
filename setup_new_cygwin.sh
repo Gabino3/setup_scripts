65c279aad5cff36664524b272f62fe9925d0e031
gabino3 <gdabdoub3@gmail.com>
2017-07-24 14:57:42 -0700

    add bind-utils to cygwin setup scropt

--- setup_new_cygwin.sh
+++ setup_new_cygwin.sh
@@ -2,7 +2,7 @@
 echo "need wget, tar, and bzip2 packages installed already"
 wget rawgit.com/transcode-open/apt-cyg/master/apt-cyg -P /bin/
 chmod +x /bin/apt-cyg
-apt-cyg install git bash-completion ca-certificates curl git python openssh nano
+apt-cyg install git bash-completion ca-certificates curl git python openssh nano bind-utils
 git clone https://github.com/karlin/mintty-colors-solarized
 chmod +x mintty-solarized-dark.sh
 echo "${HOME}/mintty-colors-solarized/mintty-solarized-dark.sh" >> ${HOME}/.bashrc
