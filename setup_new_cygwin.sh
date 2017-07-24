8aa61e3ec5a500d843df44bb08daba804448601c
gabino3 <gdabdoub3@gmail.com>
2017-07-24 14:40:46 -0700

    add setup script for cygwin

---
+++ setup_new_cygwin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "need wget, tar, and bzip2 packages installed already"
+wget rawgit.com/transcode-open/apt-cyg/master/apt-cyg -P /bin/
+chmod +x /bin/apt-cyg
+apt-cyg install git bash-completion ca-certificates curl git python openssh nano
+git clone https://github.com/karlin/mintty-colors-solarized
+chmod +x mintty-solarized-dark.sh
+echo "${HOME}/mintty-colors-solarized/mintty-solarized-dark.sh" >> ${HOME}/.bashrc
