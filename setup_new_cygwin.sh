ae568b813a0bb075aca535efa3a640b0defd8238
Gabino3 <gdabdoub3@gmail.com>
2017-07-24 18:42:44 -0700

    fix pathing issue for solarized colors

--- setup_new_cygwin.sh
+++ setup_new_cygwin.sh
@@ -3,6 +3,6 @@ echo "need wget, tar, and bzip2 packages installed already"
 wget rawgit.com/transcode-open/apt-cyg/master/apt-cyg -P /bin/
 chmod +x /bin/apt-cyg
 apt-cyg install git bash-completion ca-certificates curl git python openssh nano bind-utils
-git clone https://github.com/karlin/mintty-colors-solarized
-chmod +x mintty-solarized-dark.sh
+git clone https://github.com/karlin/mintty-colors-solarized ${HOME}/mintty-colors-solarized
+chmod +x ${HOME}/mintty-colors-solarized/mintty-solarized-dark.sh
 echo "${HOME}/mintty-colors-solarized/mintty-solarized-dark.sh" >> ${HOME}/.bashrc
