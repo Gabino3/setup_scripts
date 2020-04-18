f04872da675f5bfda50739f6774ce9111f75e4c5
Gabino3 <gdabdoub3@gmail.com>
2020-04-17 18:28:59 -0700

    Update bash_wsl.sh

--- bash_wsl.sh
+++ bash_wsl.sh
@@ -2,3 +2,22 @@ alias python="python3"
 
 eval $(ssh-agent -s) > /dev/null
 ssh-add ~/.ssh/github > /dev/null 2>&1
+
+#shortcut to go repos
+g() {
+ if [[ -d "/mnt/c/Users/nino/go/src/github.com/Gabino3/$@" ]]; then
+        command cd "/mnt/c/Users/nino/go/src/github.com/Gabino3/$@"
+ else
+        echo "no such dir"
+ fi
+}
+
+#In /etc/bash_completion.d/g
+_g()
+{
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    IFS=$'\n' tmp=( $(compgen -W "$(ls /mnt/c/Users/nino/go/src/github.com/Gabino3/ )" -- $cur))
+    COMPREPLY=( "${tmp[@]// /\ }" )
+}
+
+complete -F _g g
